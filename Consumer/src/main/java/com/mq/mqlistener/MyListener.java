de85d582356cf234aca6241523a23c1057ef00bc
风亦未止 <769522974@qq.com>
2022-07-08 23:28:57 +0800

    高级特性-继续学习

---
+++ Consumer/src/main/java/com/mq/mqlistener/MyListener.java
@@ -0,0 +1,25 @@
+package com.mq.mqlistener;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 风亦未止
+ * @date 2022/7/8 23:10
+ */
+@Component
+public class MyListener {
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = ""),
+            exchange = @Exchange(name = "",type = ExchangeTypes.TOPIC),
+            key = {"order.#"}
+    ))
+    public void myListener(){
+
+    }
+}
